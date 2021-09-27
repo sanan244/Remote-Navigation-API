56d6d9dad62acf1f6f97f44d18f8b250d0e9f17b
sanan244 <76133001+sanan244@users.noreply.github.com>
2021-09-27 15:02:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-var request   = require('request');
+
 var mysql = require('mysql');
+var lat = 0;
+var lon = 1;
+var con = mysql.createConnection({  
+    host: "localhost",   
+    user: "root",  
+    password: "",  
+    database: "root"  
+    });  
+    con.connect(function(err) {  
+    if (err) throw err;  
+    console.log("Connected!");  
+    var sql = "SELECT * FROM coordinates WHERE `id` =(SELECT MAX(id) FROM coordinates)";  
+    var values = [  
+    [ JSON.stringify(lat),JSON.stringify(lon) ],  
+    ];  
+    con.query(sql, function (err, result) {  
+    if (err) throw err;  
+    Object.keys(result).forEach(function(key) {
+        var row = result[key];
+        console.log(row)});
+    //console.log("Number of records inserted: " + result.affectedRows);
+
+});  
+});
